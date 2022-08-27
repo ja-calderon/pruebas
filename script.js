5880953a6feb4c3bbca9533862f55f183c124d45
ja-calderon <105570149+ja-calderon@users.noreply.github.com>
2022-08-26 23:57:55 -0300

    curso finalizado

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import checkComplete from "./components/checkComplete.js"; 
+import deleteIcon from "./components/deleteIcon.js";
 ( () => {
 const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
@@ -9,30 +11,16 @@ const createTask = (evento) => {
     task.classList.add("card");
     input.value = "";
     const taskContent = document.createElement("div");
-    taskContent.appendChild(checkComplete());
+   
     const titleTask = document.createElement("span");
     titleTask.classList.add("task");
     titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-    const content = `
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
 };
 
 btn.addEventListener("click", createTask)
-
-const checkComplete = () =>{
-   const i = document.createElement("i");
-   i.classList.add("far", "fa-check-square", "icon");
-   i. addEventListener("click", completeTask)
-   return i;
-};
-
-const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-};
 })();
